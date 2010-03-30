fc7699132f7172c8a589d827c44274c2e608c59f
Wichert Akkerman <wichert@wiggy.net>
2010-03-30 15:40:37 +0000

    Override the form wrapper as well

--- plonetheme/nuplone/z3cform/templates.py
+++ plonetheme/nuplone/z3cform/templates.py
@@ -1,12 +1,19 @@
 import os.path
+from plone.z3cform.interfaces import IFormWrapper
 from plone.z3cform.interfaces import IWrappedForm
 from plone.z3cform.templates import FormTemplateFactory
+from plone.z3cform.templates import ZopeTwoFormTemplateFactory
 from plonetheme.nuplone.z3cform.interfaces import INuPloneFormLayer
 from z3c.form.error import ErrorViewTemplateFactory
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
 
-layout_factory = FormTemplateFactory(
+layout_factory = ZopeTwoFormTemplateFactory(
+            os.path.join(TEMPLATE_DIR, "layout.pt"),
+            form=IFormWrapper, 
+            request=INuPloneFormLayer)
+
+wrapped_form_factory = FormTemplateFactory(
             os.path.join(TEMPLATE_DIR, "wrappedform.pt"),
             form=IWrappedForm,
             request=INuPloneFormLayer)
