cee39b8949925217c1f5fe9dbd40e20ce128252d
Wichert Akkerman <wichert@wiggy.net>
2010-04-14 10:28:28 +0000

    Lets use Chameleon to render things

--- plonetheme/nuplone/tiles/tales.py
+++ plonetheme/nuplone/tiles/tales.py
@@ -32,3 +32,39 @@ try:
 except ImportError:
     pass
 
+# z3c.pt 
+import re
+from chameleon.zpt.expressions import ExpressionTranslator
+from chameleon.zpt.interfaces import IExpressionTranslator
+from chameleon.core import types
+from zope.interface import implements
+
+class SimpleLiteral(unicode):
+    def __html__(self):
+        return unicode(self)
+
+def _lookup_tile(context, request, name):
+    tile=queryMultiAdapter((context, request), Interface, name=name)
+    if tile is None:
+        log.warn("Detected reference to non-existing tile '%s' for context %r",
+                 name, context)
+        return u""
+
+    return SimpleLiteral(tile())
+
+class TileTranslator(ExpressionTranslator):
+    implements(IExpressionTranslator)
+
+    symbol = "_lookup_tile"
+    re_name = re.compile(r"^[a-z]+$")
+
+    def translate(self, string, escape=None):
+        if not string:
+            return None
+        string=string.strip()
+        if self.re_name.match(string) is None:
+            raise SyntaxError(string)
+        value=types.value("%s(context, request, '%s')" % (self.symbol, string))
+        value.symbol_mapping[self.symbol]=_lookup_tile
+        return value
+
