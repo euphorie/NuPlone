6b784f17ec49621766c6e921a5fcea28f93aa2a2
Wichert Akkerman <wichert@wiggy.net>
2010-04-14 10:56:59 +0000

    Now that we use chameleon we can drop all the silly workarounds for ZPT problems

--- plonetheme/nuplone/tiles/tales.py
+++ plonetheme/nuplone/tiles/tales.py
@@ -1,43 +1,14 @@
 import logging
+import re
 from zope.interface import Interface
 from zope.component import queryMultiAdapter
-from zope.tales.expressions import StringExpr
-
-log = logging.getLogger(__name__)
-
-class TileExpression(StringExpr):
-    def __call__(self, econtext):
-        name=super(TileExpression, self).__call__(econtext)
-        context=econtext.vars["context"]
-        request=econtext.vars["request"]
-
-        tile=queryMultiAdapter((context, request), Interface, name=name)
-        if tile is None:
-            log.warn("Detected reference to non-existing tile '%s' for context %r",
-                     name, context)
-            return u""
-
-        return tile()
-
-# Zope 2.12 does not support zcml-registration of expression types.
-try:
-    from Products.Five.browser import pagetemplatefile
-    pagetemplatefile.getEngine().registerType("tile", TileExpression)
-except ImportError:
-    pass
-
-try:
-    from Products.PageTemplates import Expressions
-    Expressions.getEngine().registerType("tile", TileExpression)
-except ImportError:
-    pass
-
-# z3c.pt 
-import re
 from chameleon.zpt.expressions import ExpressionTranslator
 from chameleon.zpt.interfaces import IExpressionTranslator
 from chameleon.core import types
 from zope.interface import implements
+
+log = logging.getLogger(__name__)
+
 
 class SimpleLiteral(unicode):
     def __html__(self):
