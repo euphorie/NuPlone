1b8ff970cd08d882818ae280c0bb5fef169061d8
Wichert Akkerman <wichert@wiggy.net>
2010-04-15 14:37:57 +0000

    Allow use of z3 permissions as well

--- plonetheme/nuplone/skin/tales.py
+++ plonetheme/nuplone/skin/tales.py
@@ -1,23 +1,28 @@
 import logging
 import re
 from AccessControl import getSecurityManager
+from zope.component import queryUtility
 from chameleon.zpt.expressions import ExpressionTranslator
 from chameleon.zpt.interfaces import IExpressionTranslator
 from chameleon.core import types
 from zope.interface import implements
+from zope.security.interfaces import IPermission
 
 log = logging.getLogger(__name__)
 
 
 def _checkPermission(context, permission):
-    return getSecurityManager().checkPermission(permission, context)
+    title = queryUtility(IPermission, name=permission)
+    if title is None:
+        title=permission
+    return getSecurityManager().checkPermission(title, context)
 
 
 class PermissionTranslator(ExpressionTranslator):
     implements(IExpressionTranslator)
 
     symbol = "_checkPermission"
-    re_name = re.compile(r"^[a-z]+$")
+    re_name = re.compile(r"^[A-z _-]+$")
 
     def translate(self, string, escape=None):
         if not string:
