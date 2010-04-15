36791a9a71915f027bc91f77fe4cc1e18b802875
Wichert Akkerman <wichert@wiggy.net>
2010-04-15 16:01:57 +0000

    Drop z3 permission support again. This is Zope 2 and these will never go away so stop the possible confusion, and remove a small performance penalty as a bonus

--- plonetheme/nuplone/skin/tales.py
+++ plonetheme/nuplone/skin/tales.py
@@ -1,20 +1,15 @@
 import logging
 import re
 from AccessControl import getSecurityManager
-from zope.component import queryUtility
 from chameleon.zpt.expressions import ExpressionTranslator
 from chameleon.zpt.interfaces import IExpressionTranslator
 from chameleon.core import types
 from zope.interface import implements
-from zope.security.interfaces import IPermission
 
 log = logging.getLogger(__name__)
 
 
 def _checkPermission(context, permission):
-    title = queryUtility(IPermission, name=permission)
-    if title is None:
-        title=permission
     return getSecurityManager().checkPermission(title, context)
 
 
