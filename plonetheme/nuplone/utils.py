a3ac533ccd41d75e1cd1508c121d3f881d348d54
Wichert Akkerman <wichert@wiggy.net>
2010-04-19 09:53:06 +0000

    A few more utility methods

--- plonetheme/nuplone/utils.py
+++ plonetheme/nuplone/utils.py
@@ -1,10 +1,22 @@
-from plone.app.layout.navigation.interfaces import INavigationRoot
 from Acquisition import aq_inner
 from Acquisition import aq_chain
+from AccessControl import getSecurityManager
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+def getPortal(context):
+    for obj in aq_chain(aq_inner(context)):
+        if IPloneSiteRoot.providedBy(obj):
+            return obj
+
 
 def getNavigationRoot(context):
     for obj in aq_chain(aq_inner(context)):
         if INavigationRoot.providedBy(obj):
             break
     return obj
+
+
+def checkPermission(context, permission):
+    return getSecurityManager().checkPermission(permission, context)
 
