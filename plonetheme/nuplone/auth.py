9d7af8c10434420700f686f95ac251154ec6255f
Wichert Akkerman <wichert@wiggy.net>
2010-04-20 15:57:42 +0000

    Simplify: delegate all work to the exception view, so our PAS plugin can sit back and do nothing

--- plonetheme/nuplone/auth.py
+++ plonetheme/nuplone/auth.py
@@ -1,16 +1,13 @@
-import urllib
 from zope.interface import implements
 from App.class_init import default__class_init__ as InitializeClass
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 from Products.PluggableAuthService.interfaces.plugins import IChallengePlugin
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
-from plonetheme.nuplone.utils import getNavigationRoot
 
 
 class LoginChallenger(BasePlugin):
-    """Simple login challenger which redirects to the login form.
-    This can also be done as a setting on the cookie_auth plugin, but we
-    do not do that so other themes can still work.
+    """Simple login challenger which does nothing, allowing the
+    Unauthorized exception view to be used instead.
     """
     meta_type = "NuPlone Login Challenger"
     id = "nuplone-challenger"
@@ -20,19 +17,6 @@ class LoginChallenger(BasePlugin):
         if not NuPloneSkin.providedBy(request):
             return False
 
-        came_from=None
-        for url in [ "ACTUAL_URL", "VIRTUAL_URL", "URL" ]:
-            came_from=request.get(url)
-            if came_from:
-                break
-
-        navroot=getNavigationRoot(self)
-        if came_from:
-            url="%s/@@login?%s" % (navroot.absolute_url(),
-                                   urllib.urlencode(dict(came_from=came_from)))
-        else:
-            url="%s/@@login" % navroot.absolute_url()
-        response.redirect(url, lock=True)
         return True
 
 
