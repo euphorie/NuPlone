9b1802cf58a155704663f1f94a5b5217bf3fde44
Wichert Akkerman <wichert@wiggy.net>
2010-07-15 16:51:27 +0000

    Implement password reset.

--- plonetheme/nuplone/skin/pwreminder.py
+++ plonetheme/nuplone/skin/pwreminder.py
@@ -13,9 +13,18 @@ from z3c.form.button import buttonAndHandler
 from Products.statusmessages.interfaces import IStatusMessage
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plonetheme.nuplone.utils import createEmailTo
+from Products.PasswordResetTool.PasswordResetTool import ExpiredRequestError
+from Products.PasswordResetTool.PasswordResetTool import InvalidRequestError
 
 
 log = logging.getLogger(__name__)
+
+
+class IRequestPasswordReset(form.Schema):
+    login = schema.TextLine(
+            title=_("label_login", default=u"Login"),
+            required=True)
+
 
 
 class IPasswordReset(form.Schema):
@@ -23,8 +32,13 @@ class IPasswordReset(form.Schema):
             title=_("label_login", default=u"Login"),
             required=True)
 
+    password = schema.Password(
+            title = _("label_password", default=u"Password"),
+            required = True)
 
-class PasswordReminderForm(form.SchemaForm):
+
+
+class RequestPasswordForm(form.SchemaForm):
     grok.context(ISiteRoot)
     grok.name("request-password-reset")
     grok.require("zope2.Public")
@@ -32,7 +46,7 @@ class PasswordReminderForm(form.SchemaForm):
     email_template = ViewPageTemplateFile("templates/pwreset-email.pt")
 
     ignoreContext = True
-    schema = IPasswordReset
+    schema = IRequestPasswordReset
     label = _(u"header_password_reset", default="Reset password")
     default_fieldset_label = None
     description = _(u"into_password_reset",
@@ -46,31 +60,31 @@ class PasswordReminderForm(form.SchemaForm):
     @buttonAndHandler(_("button_send", default="Send"), name="send")
     def handleSend(self, action):
         flash=IStatusMessage(self.request).addStatusMessage
-        data, errors = self.extractData()
+        data, errors=self.extractData()
         if errors:
-            self.status = self.formErrorsMessage
+            self.status=self.formErrorsMessage
             return
 
-        pas = getToolByName(self.context, "acl_users")
-        mt = getToolByName(self.context, "portal_membership")
-        user = pas.getUser(data["login"])
+        pas=getToolByName(self.context, "acl_users")
+        mt=getToolByName(self.context, "portal_membership")
+        user=pas.getUser(data["login"])
         if user is None:
             log.info("Password reset request for unknown user %s" % data["login"])
             flash(_("error_pwreset_unknown_user", default=u"Unknown username."), "error")
             return
 
-        member = mt.getMemberById(user.getId())
-        email_address = member.getProperty("email")
+        member=mt.getMemberById(user.getId())
+        email_address=member.getProperty("email")
         if not email_address:
             flash(_("error_pwreset_no_email", default=u"No known email address for this user."), "error")
             return
 
-        ppr = getToolByName(self.context, "portal_password_reset")
-        reset = ppr.requestReset(user.getId())
-        portal_url = aq_inner(self.context).absolute_url()
-        reset_url = "%s/@@reset-password/%s" % (portal_url, reset["randomstring"])
+        ppr=getToolByName(self.context, "portal_password_reset")
+        reset=ppr.requestReset(user.getId())
+        portal_url=aq_inner(self.context).absolute_url()
+        reset_url="%s/@@reset-password/%s" % (portal_url, reset["randomstring"])
 
-        data["site"] = self.context.title
+        data["site"]=self.context.title
         subject=_(u"password_reset_subject", default=u"Password reset for ${site}", mapping=data)
         subject=translate(subject, context=self.request)
         body=self.email_template(reset_url=reset_url, login=data["login"], site=self.context.title)
@@ -99,12 +113,48 @@ class PasswordReminderForm(form.SchemaForm):
 
 
 
-class PasswordReset(grok.View):
+class PasswordReset(form.SchemaForm):
     grok.context(ISiteRoot)
     grok.name("reset-password")
     grok.require("zope2.Public")
 
-    def render(self):
-        return ""
+    randomstring = None
 
+    ignoreContext = True
+    schema = IPasswordReset
+    label = _(u"header_password_reset", default=u"Reset password")
+    description = _(u"intro_password_reset",
+            default=u"Please fill out the form below to set your password.")
+    default_fieldset_label = None
+
+    def publishTraverse(self, request, name):
+        if self.randomstring is not None:
+            raise KeyError(name)
+
+        ppr=getToolByName(self.context, "portal_password_reset")
+        try:
+            ppr.verifyKey(name)
+        except (InvalidRequestError, ExpiredRequestError):
+            raise KeyError(name)
+
+        self.randomstring=name
+        return self
+
+
+    @buttonAndHandler(_("button_change", default="Change"), name="change")
+    def handleChange(self, action):
+        data, errors=self.extractData()
+        if errors:
+            self.status=self.formErrorsMessage
+            return
+        
+        flash=IStatusMessage(self.request).addStatusMessage
+        pas=getToolByName(self.context, "acl_users")
+        ppr=getToolByName(self.context, "portal_password_reset")
+        user=pas.getUser(data["login"])
+        ppr.resetPassword(user.getId(), self.randomstring, data["password"])
+
+        flash(_("password_reset", u"Your password has been reset."), "success")
+        portal_url=aq_inner(self.context).absolute_url()
+        self.request.response.redirect("%s/@@login" % portal_url)
 
