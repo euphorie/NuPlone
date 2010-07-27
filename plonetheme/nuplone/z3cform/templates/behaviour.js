a2eda514464fb6a001f40ab21241b622c5dca821
Wichert Akkerman <wichert@wiggy.net>
2010-07-27 14:49:23 +0000

    Attempt at javascript cleanup in preparation of some debugging.

--- plonetheme/nuplone/z3cform/templates/behaviour.js
+++ plonetheme/nuplone/z3cform/templates/behaviour.js
@@ -1,123 +1,158 @@
-(function($) {
-    function renumberElement(el, attr, i) {
+var z3cform = {
+    renumberElement: function (el, attr, i) {
         var $el = $(el),
             buf = $el.attr(attr);
         if (buf) {
             $el.attr(attr, buf.replace(/[0-9]+/, i));
         }
-    }
+    },
 
-    function renumber($fieldset) {
+    renumber: function($fieldset) {
         var $fieldsets = $fieldset.children("fieldset"),
             i;
         $(":input:first", $fieldset).val($fieldsets.length);
 
         for (i=0; i<$fieldsets.length; i++) {
             $("label, :input", $fieldsets[i]).each(function() {
-                renumberElement(this, "for", i);
-                renumberElement(this, "id", i);
-                renumberElement(this, "name", i);
+                z3cform.renumberElement(this, "for", i);
+                z3cform.renumberElement(this, "id", i);
+                z3cform.renumberElement(this, "name", i);
             });
         }
-    }
+    },
 
-
-    $(".multiWidget > button.add").live("click", function() {
-        var $button = $(this),
-            url = $button.val();
-        $.get(url, function(data, status) {
-            var $fragment=$(data);
-            $button.before($fragment);
-            renumber($button.parent());
+    initialiseMultiwidget: function() {
+        $(".multiWidget > button.add").live("click", function() {
+            var $button = $(this),
+                url = $button.val();
+            $.get(url, function(data, status) {
+                var $fragment=$(data);
+                $button.before($fragment);
+                z3cform.renumber($button.parent());
+            });
         });
-    });
 
-    $(".multiWidget > fieldset > button.remove").live("click", function() {
-        var $button = $(this),
-            $fieldset = $button.parent(),
-            $root = $fieldset.parent();
+        $(".multiWidget > fieldset > button.remove").live("click", function() {
+            var $button = $(this),
+                $fieldset = $button.parent(),
+                $root = $fieldset.parent();
 
-        $fieldset.remove();
-        renumber($root);
-    });
+            $fieldset.remove();
+            z3cform.renumber($root);
+        });
+    },
 
-    $(window).load(function() {
-        var $rich_fields = $("textarea.rich");
+    onTinyActivate: function(ed) {
+        var $wrapper = $("#frameWrapper"),
+            $controls;
 
-        function onTinyActivate(ed) {
-            var $wrapper = $("#frameWrapper"),
-                $controls;
-
-            if (!$wrapper.length) {
-                $wrapper=$("<p/>").attr("id", "frameWrapper").appendTo(document.body);
-            }
-
-            function triggerActive() {
-                try {
-                    $("#tinyControls").get(0).contentWindow.activate(ed);
-                } catch(e) {
-                }
-            }
-
-            $controls = $("#tinyControls");
-            if (!$controls.length) {
-                $("<iframe/>")
-                    .attr("id", "tinyControls")
-                    .attr("src", plone.context_url+"/@@tiny-controls")
-                    .css("position", "absolute")
-                    .css("top", "0px")
-                    .css("left", "0px")
-                    .css("z-index", "1100")
-                    .appendTo($wrapper)
-                    .load(triggerActive);
-            } else {
-                triggerActive();
-            }
-
-            if (!$("body").hasClass("edit")) {
-                $("body").addClass("edit");
-            }
-
-            $controls = $("#linkFrame");
-            if (!$controls.length) {
-                $("<iframe/>")
-                    .attr("id", "linkFrame")
-                    .attr("src", plone.context_url+"/@@edit-link")
-                    .attr("frameborder", "0")
-                    .css("z-index", "1100")
-                    .css("height", "250px")
-                    .css("width", "510px")
-                    .css("margin-left", "-255px")
-                    .css("left", "50%")
-                    .css("position", "absolute")
-                    .css("top", "70px")
-                    .css("-webkit-box-shadow", "0 0 15px rgba(0,0,0,0.5)")
-                    .css("-moz-box-shadow", "0 0 15px rgba(0,0,0,0.5)")
-                    .css("box-shadow", "0 0 15px rgba(0,0,0,0.5)")
-                    .css("display", "none")
-                    .appendTo($wrapper);
-            }
+        if (!$wrapper.length) {
+            $wrapper=$("<p/>").attr("id", "frameWrapper").appendTo(document.body);
         }
 
-	function onTinyDeactivate(ed) {
+        function triggerActive() {
             try {
-                $("#tinyControls").get(0).contentWindow.deactivate(ed);
+                $("#tinyControls").get(0).contentWindow.activate(ed);
             } catch(e) {
             }
-	}
-
-        function onTinySetup(ed) {
-            ed.onDblClick.add(function(ed, e) {
-                if (e.target.nodeName.toLowerCase()=="a") {
-                    $("#linkFrame").get(0).contentWindow.show(e.target);
-                }
-            });
-
-	    ed.onActivate.add(onTinyActivate);
-	    ed.onDeactivate.add(onTinyDeactivate);
         }
 
-        $(":input").live("focus", function() {
+        $controls = $("#tinyControls");
+        if (!$controls.length) {
+            $("<iframe/>")
+                .attr("id", "tinyControls")
+                .attr("src", plone.context_url+"/@@tiny-controls")
+                .css("position", "absolute")
+                .css("top", "0px")
+                .css("left", "0px")
+                .css("z-index", "1100")
+                .appendTo($wrapper)
+                .load(triggerActive);
+        } else {
+            triggerActive();
+        }
+
+        if (!$("body").hasClass("edit")) {
+            $("body").addClass("edit");
+        }
+
+        $controls = $("#linkFrame");
+        if (!$controls.length) {
+            $("<iframe/>")
+                .attr("id", "linkFrame")
+                .attr("src", plone.context_url+"/@@edit-link")
+                .attr("frameborder", "0")
+                .css("z-index", "1100")
+                .css("height", "250px")
+                .css("width", "510px")
+                .css("margin-left", "-255px")
+                .css("left", "50%")
+                .css("position", "absolute")
+                .css("top", "70px")
+                .css("-webkit-box-shadow", "0 0 15px rgba(0,0,0,0.5)")
+                .css("-moz-box-shadow", "0 0 15px rgba(0,0,0,0.5)")
+                .css("box-shadow", "0 0 15px rgba(0,0,0,0.5)")
+                .css("display", "none")
+                .appendTo($wrapper);
+        }
+    },
+
+    onTinyDeactivate: function(ed) {
+        try {
+            $("#tinyControls").get(0).contentWindow.deactivate(ed);
+        } catch(e) {
+        }
+    },
+
+    onTinySetup: function(ed) {
+        ed.onDblClick.add(function(ed, e) {
+            if (e.target.nodeName.toLowerCase()=="a") {
+                $("#linkFrame").get(0).contentWindow.show(e.target);
+            }
+        });
+
+        ed.onActivate.add(z3cform.onTinyActivate);
+        ed.onDeactivate.add(z3cform.onTinyDeactivate);
+    },
+
+    onFormSubmit: function() {
+        $("div.rich.input", this).each(function() {
+            var $field = $(this),
+                id = $field.attr("id"),
+                value = tinyMCE.get(id).getContent(),
+                name = $field.data("z3cform.name");
+            if (name) {
+                $("<input/>")
+                    .attr("type", "hidden")
+                    .attr("name", name)
+                    .val(value)
+                    .insertBefore($field);
+            }
+        });
+        return true;
+    },
+
+    initialiseRichTextEditor: function(root) {
+        if ($("textarea.rich", root).length===0) {
+            return;
+        }
+
+        tinyMCE.init({mode: "none",
+                      theme: "dummy",
+                      plugins: "linefield",
+                      element_format: "xhtml",
+                      fix_list_elements: true,
+                      fix_table_elements: true,
+                      entity_encoding: "raw",
+                      content_editable: true,
+                      forced_root_block: null,
+                      setup: onTinySetup
+                     });
+
+        // Make sure to deactivate tiny when a non-tiny field gets the
+        // focus. This will automatically disable the relevant actions
+        // in the toolbar.
+        $(":input", root).focus(function() {
             try {
                 tinymce.EditorManager._setActive(null);
                 onTinyDeactivate();
@@ -125,21 +160,9 @@
             }
         });
 
-	if ($rich_fields.length) {
-	    tinyMCE.init({mode: "none",
-			  theme: "dummy",
-			  plugins: "linefield",
-			  element_format: "xhtml",
-			  fix_list_elements: true,
-			  fix_table_elements: true,
-			  entity_encoding: "raw",
-			  content_editable: true,
-			  forced_root_block: null,
-			  setup: onTinySetup
-			 });
-	}
-
-        $rich_fields.each(function() {
+        // Replace rich textareas with a div, and activate a tinyMCE editor
+        // for them.
+        $("textarea.rich", root).each(function() {
             var $textarea = $(this),
                 id = $textarea.attr("id"),
                 $div = $("<div/>");
@@ -154,7 +177,9 @@
             tinyMCE.execCommand("mceAddControl", false, id);
         });
 
-        $(".rich:not(:input)").each(function() {
+        // Check for focus and existing content to determine visibility
+        // of any superimposed labels
+        $(".rich:not(:input)", root).each(function() {
             var $el = $(this),
                 $component = $el.closest(".richInput");
 
@@ -172,32 +197,30 @@
 
             update();
 
-            $el.bind("focus", function() {
-                    $component.data("mapal.timer.rich", setInterval(update, 100));
+            $el.bind("focus.z3cform", function() {
+                    $component.data("z3cform.timer.rich", setInterval(update, 100));
                 })
-                .bind("blur", function() {
+                .bind("blur.z3cform", function() {
                     update();
-                    clearInterval($component.data("mapal.timer.rich"));
+                    clearInterval($component.data("z3cform.timer.rich"));
                 });
         });
 
-    });
+        $("form", root).bind("submit.z3cform", z3cform.onFormSubmit);
+    },
 
-    $("form").live("submit", function() {
-        $("div.rich.input", this).each(function() {
-            var $field = $(this),
-                id = $field.attr("id"),
-                value = tinyMCE.get(id).getContent(),
-                name = $field.data("z3cform.name");
-            if (name) {
-                $("<input/>")
-                    .attr("type", "hidden")
-                    .attr("name", name)
-                    .val(value)
-                    .insertBefore($field);
-            }
-        });
-        return true;
-    });
-})(jQuery);
+    initContent: function(root) {
+        initialiseRichTextEditor(root);
+    },
+
+    init: function() {
+         z3cform.initialiseMultiwidget();
+    }
+};
+
+
+z3cform.init()
+$(window).load(function() {
+    z3cform.initContent(document);
+});
 
