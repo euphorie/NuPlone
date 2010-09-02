e3ec88f24b2ee08e98e3e3ef3b6c0cb230543be9
Wichert Akkerman <wichert@wiggy.net>
2010-09-02 13:46:59 +0000

    Delete old leftovers.

--- docs/changes.rst
+++ docs/changes.rst
@@ -4,7 +4,9 @@ Changelog
 1.0 - Unreleased
 ----------------
 
-* ...
+* Delete some old copy/paste leftovers from `Euphorie
+  <http://pypi.python.org/pypi/Euphorie>`_.
+  [wichert]
 
 
 1.0a1 - August 31, 2010
--- plonetheme/nuplone/__init__.py
+++ plonetheme/nuplone/__init__.py
@@ -2,17 +2,3 @@ from zope.i18nmessageid import MessageFactory as mf
 MessageFactory = mf("nuplone")
 del mf
 
-
-def initialize(context):
-    from AccessControl.Permissions import manage_users as ManageUsers
-    from Products.PluggableAuthService.PluggableAuthService import registerMultiPlugin
-    from euphorie.client import authentication
-
-    registerMultiPlugin(authentication.EuphorieAccountPlugin.meta_type)
-    context.registerClass(authentication.EuphorieAccountPlugin,
-                          permission=ManageUsers,
-                                constructors=
-                                        (authentication.manage_addEuphorieAccountPlugin,
-                                        authentication.addEuphorieAccountPlugin),
-                                visibility=None)
-
