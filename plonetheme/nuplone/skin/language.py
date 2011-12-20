6bf5b24d36753f36c738dc8b79b719be5872d0f8
Wichert Akkerman <wichert@wiggy.net>
2011-12-20 19:31:40 +0100

    PEP8

--- plonetheme/nuplone/skin/language.py
+++ plonetheme/nuplone/skin/language.py
@@ -9,21 +9,23 @@ from Products.statusmessages.interfaces import IStatusMessage
 
 class SwitchLanguage(grok.View):
     grok.context(Interface)
-    grok.require("zope2.View")
-    grok.name("switch-language")
+    grok.require('zope2.View')
+    grok.name('switch-language')
 
     def render(self):
-        flash=IStatusMessage(self.request).addStatusMessage
+        flash = IStatusMessage(self.request).addStatusMessage
         if setLanguage(self.request, self.context):
-            flash(_("message_switch_language", default=u"Language updated"), "success")
+            flash(_('message_switch_language',
+                default=u'Language updated'), 'success')
         else:
-            flash(_("message_switch_language_error", default=u"Failed to switch language"), "error")
+            flash(_('message_switch_language_error',
+                default=u'Failed to switch language'), 'error')
 
-        next_url=self.request.get("came_from")
+        next_url = self.request.get('came_from')
         if not next_url:
-            next_url=self.request.environ.get("HTTP_REFERER")
-        if not next_url or not next_url.startswith(getPortal(self.context).absolute_url()):
-            next_url=aq_inner(self.context).absolute_url()
+            next_url = self.request.environ.get('HTTP_REFERER')
+        if not next_url or not \
+                next_url.startswith(getPortal(self.context).absolute_url()):
+            next_url = aq_inner(self.context).absolute_url()
 
         self.request.response.redirect(next_url)
-
