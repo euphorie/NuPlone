3d1b4cc33e2bc0bc28bdddeaa47e1ccc29ea8a53
Wichert Akkerman <wichert@wiggy.net>
2012-12-07 13:29:35 +0100

    Add everything needed to run editors in the prototype.

---
+++ Prototype/serve.py
@@ -0,0 +1,19 @@
+import os
+import BaseHTTPServer
+import SimpleHTTPServer
+
+
+class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    do_POST = SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET
+
+    def guess_type(self, path):
+        if '@@' in path:
+            return 'text/html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.guess_type(self, path)
+
+
+if __name__ == '__main__':
+    directory = os.path.dirname(__file__)
+    os.chdir(os.path.join(directory, os.pardir))
+    print 'You can view the prototype at http://localhost:8000/Prototype/'
+    BaseHTTPServer.test(RequestHandler, BaseHTTPServer.HTTPServer)
