1c5be8fa4aac689f5c2fdaaf23157aa19e574cae
JC Brand <jc@opkode.com>
2013-01-14 10:36:18 +0200

    PEP8 (wrap at 80 chars and spaces around equal signs)

--- plonetheme/nuplone/skin/sitemenu.py
+++ plonetheme/nuplone/skin/sitemenu.py
@@ -12,7 +12,6 @@ from Products.CMFCore.interfaces import ISiteRoot
 from OFS.interfaces import ICopyContainer
 from OFS.interfaces import ICopySource
 
-
 grok.templatedir("templates")
 
 
@@ -26,21 +25,19 @@ class Sitemenu(grok.View):
     def settings_url(self):
         return "%s/@@settings" % self.navroot_url
 
-
     def update(self):
-        self.view_type=self.request.get("view_type", "view")
-        self.actions=self.actions()
-
+        self.view_type = self.request.get("view_type", "view")
+        self.actions = self.actions()
 
     def actions(self):
         """Helper method to generate the contents for the actions menu.
         """
-        menu={"title": _("menu_actions", default=u"Actions")}
-        children=menu["children"]=[]
-        submenu=self.factories()
+        menu = {"title": _("menu_actions", default=u"Actions")}
+        children = menu["children"] = []
+        submenu = self.factories()
         if submenu:
             children.append(submenu)
-        submenu=self.organise()
+        submenu = self.organise()
         if submenu:
             children.append(submenu)
 
@@ -49,14 +46,13 @@ class Sitemenu(grok.View):
         else:
             return None
 
-
     def factories(self):
         """Helper method to generate the menu items for creating new content.
         If no content can be created None is returned.
         """
-        menu={"title": _("menu_add_new", default=u"Add new")}
-        children=menu["children"]=[]
-        actions=getFactoriesInContext(self.context)
+        menu = {"title": _("menu_add_new", default=u"Add new")}
+        children = menu["children"] = []
+        actions = getFactoriesInContext(self.context)
         actions.sort(key=lambda x: x.title)
         for action in actions:
             children.append({"title": action.title,
@@ -68,25 +64,29 @@ class Sitemenu(grok.View):
         else:
             return None
 
-
     def organise(self):
         """Helper method to generate the menu items for organising content
         (copy/paste/etc.).
 
         If no organising actions are available None is returned.
         """
-        context=aq_inner(self.context)
-        context_url=context.absolute_url()
-        parent=aq_parent(context)
-        is_root=ISiteRoot.providedBy(context)
-        is_copyable=not is_root and ICopySource.providedBy(context) and checkPermission(context, copy_or_move)
-        can_delete=not is_root and checkPermission(parent, delete_objects)
-        can_copy=is_copyable and context.cb_isCopyable()
-        can_cut=is_copyable and can_delete and context.cb_isMoveable()
-        can_paste=ICopyContainer.providedBy(context) and context.cb_dataValid()
+        context = aq_inner(self.context)
+        context_url = context.absolute_url()
+        parent = aq_parent(context)
+        is_root = ISiteRoot.providedBy(context)
+        is_copyable = not is_root and \
+                        ICopySource.providedBy(context) and \
+                        checkPermission(context, copy_or_move)
 
-        menu={"title": _("menu_organise", default=u"Organise")}
-        children=menu["children"]=[]
+        can_delete = not is_root and checkPermission(parent, delete_objects)
+        can_copy = is_copyable and context.cb_isCopyable()
+        can_cut = is_copyable and can_delete and context.cb_isMoveable()
+
+        can_paste = ICopyContainer.providedBy(context) and \
+                        context.cb_dataValid()
+
+        menu = {"title": _("menu_organise", default=u"Organise")}
+        children = menu["children"] = []
         if can_copy:
             children.append({"title": _("menu_copy", default=u"Copy"),
                              "url": "%s/@@copy" % context_url})
