46757a39d7d53b7d37e9451dc0c9d287f41a4ea9
JC Brand <jc@opkode.com>
2013-01-15 09:59:26 +0200

    Update sitemenu tests.
    Check that "Paste" action is not shown when user isn't permitted.

--- plonetheme/nuplone/skin/tests/test_sitemenu.py
+++ plonetheme/nuplone/skin/tests/test_sitemenu.py
@@ -1,9 +1,10 @@
 import unittest2 as unittest
-import transaction
 import zope.component
 from zope.interface import alsoProvides
+from plone.testing import z2
 from plone.app.testing import login
 from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
@@ -12,39 +13,50 @@ from plonetheme.nuplone.testing import NUPLONE_FUNCTIONAL_TESTING
 class SiteMenuTests(unittest.TestCase):
     layer = NUPLONE_FUNCTIONAL_TESTING
 
-    def testActions(self):
-        portal = self.layer["portal"]
-        request = portal.REQUEST
-        alsoProvides(request, NuPloneSkin)
-
-        login(portal, TEST_USER_NAME)
-        setRoles(portal, TEST_USER_ID, ('Manager',))
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.request = self.portal.REQUEST
+        alsoProvides(self.request, NuPloneSkin)
+        z2.login(self.layer['app']['acl_users'], SITE_OWNER_NAME)
 
         # Create two folders
-        portal.invokeFactory("Folder", "folder1")
-        folder1 = portal['folder1']
-        transaction.commit()
+        self.portal.invokeFactory("Folder", "source_folder")
+        self.source_folder = self.portal['source_folder']
 
-        portal.invokeFactory("Folder", "folder2")
-        folder2 = portal['folder2']
-        transaction.commit()
+        self.portal.invokeFactory("Folder", "dest_folder")
+        self.dest_folder = self.portal['dest_folder']
 
         # Create an object to be copied
-        folder1.invokeFactory("Document", "doc1")
-        transaction.commit()
+        self.source_folder.invokeFactory("Document", "doc")
 
+    def testActions(self):
+        login(self.portal, TEST_USER_NAME)
+        setRoles(self.portal, TEST_USER_ID, ('Contributor',))
         # Copy the object
-        cp = folder1.manage_copyObjects(ids=['doc1'])
-        request['__cp'] = cp
+        cp = self.source_folder.manage_copyObjects(ids=['doc'])
+        self.request['__cp'] = cp
 
         # We cannot paste the object in the site root
-        v = zope.component.getMultiAdapter((portal, request), name="sitemenu")
+        v = zope.component.getMultiAdapter(
+                                (self.portal, self.request), name="sitemenu")
         self.assertIsNone(v.organise())
 
         # We can however paste the object in each of the folders
-        for folder in [folder1, folder2]:
-            v = zope.component.getMultiAdapter((folder1, request), name="sitemenu")
+        for folder in [self.source_folder, self.dest_folder]:
+            v = zope.component.getMultiAdapter(
+                                (folder, self.request), name="sitemenu")
             menu = v.organise()
             self.assertIsNotNone(menu)
             children_titles = [i['title'] for i in menu['children']]
             self.assertTrue('menu_paste' in children_titles)
+
+        # Now when we give the user the Viewer role, they cannot paste anymore
+        # so we should not see "Paste" as an available action.
+        setRoles(self.portal, TEST_USER_ID, ('Viewer',))
+        for folder in [self.source_folder, self.dest_folder]:
+            v = zope.component.getMultiAdapter(
+                                (folder, self.request), name="sitemenu")
+            menu = v.organise()
+            self.assertIsNotNone(menu)
+            children_titles = [i['title'] for i in menu['children']]
+            self.assertFalse('menu_paste' in children_titles)
