euphorie/NuPlone

commit b70d69d850501a718ec7358e9162a5e0c8e73da7

Make tinymce workable in prototype.

plonetheme/nuplone/z3cform/configure.zcml
@@ -14,14 +14,14 @@
   <adapter factory=".templates.form_factory" />
 
   <browser:page
-    name="tiny-controls"
+    name="tiny-controls.html"
     for="*"
     permission="zope2.Public"
     template="templates/toolbar.pt"
     />
 
   <browser:page
-    name="edit-link"
+    name="edit-link.html"
     for="*"
     permission="cmf.ModifyPortalContent"
     template="templates/editlink.pt"

plonetheme/nuplone/z3cform/templates/behaviour.js
@@ -99,7 +99,7 @@ var z3cform = {
         if (!$("#tinyControls").length) {
             $("<iframe/>")
                 .attr("id", "tinyControls")
-                .attr("src", plone.context_url+"/@@tiny-controls")
+                .attr("src", plone.context_url+"/@@tiny-controls.html")
                 .css("position", "absolute")
                 .css("top", "0px")
                 .css("left", "0px")
@@ -110,7 +110,7 @@ var z3cform = {
         if (!$("#linkFrame").length) {
             $("<iframe/>")
                 .attr("id", "linkFrame")
-                .attr("src", plone.context_url+"/@@edit-link")
+                .attr("src", plone.context_url+"/@@edit-link.html")
                 .attr("frameborder", "0")
                 .css("z-index", "1100")
                 .css("height", "250px")

commit 2e9cce01dca14412ab3e0e0e7b1b3db9b83ba613

Fix for bug introduced in b70d69d850501a718ec7358e9162a5e0c8e73da7
Iframe is opened for edit-link.html but new grok view was only called
edit-link.

Old zcml registration of edit-link.html was also still there.

plonetheme/nuplone/z3cform/configure.zcml
@@ -20,13 +20,6 @@
     template="templates/toolbar.pt"
     />
 
-  <browser:page
-    name="edit-link.html"
-    for="*"
-    permission="cmf.ModifyPortalContent"
-    template="templates/editlink.pt"
-    />
-
   <browser:resource
     name="NuPlone.z3cform.js"
     file="templates/behaviour.js"

plonetheme/nuplone/z3cform/wysiwyg.py
@@ -23,7 +23,7 @@ class ExternalLinkSchema(form.Schema):
 class EditLink(form.SchemaForm):
     grok.context(Interface)
     grok.require("cmf.ModifyPortalContent")
-    grok.name("edit-link")
+    grok.name("edit-link.html")
     grok.template("editlink")
 
     ignoreContext = True