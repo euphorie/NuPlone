520b98a4c2d6620ea19f63ff504b1fd4b363c68e
JC Brand <jc@opkode.com>
2014-07-14 12:19:00 +0200

    Move registering of Nuplone Challenger plugin to own method

--- plonetheme/nuplone/setuphandlers.py
+++ plonetheme/nuplone/setuphandlers.py
@@ -6,18 +6,19 @@ log = logging.getLogger(__name__)
 def setupVarious(context):
     if context.readDataFile('plonetheme.nuplone.txt') is None:
         return
+    addNuploneChallengerPlugin(context)
 
-    site=context.getSite()
-    pas=site.acl_users
+def addNuploneChallengerPlugin(context):
+    site = context.getSite()
+    pas = site.acl_users
     if not pas.objectIds([LoginChallenger.meta_type]):
-        plugin=LoginChallenger("nuplone-challenger", LoginChallenger.meta_type)
+        plugin = LoginChallenger("nuplone-challenger", LoginChallenger.meta_type)
         pas._setObject(plugin.getId(), plugin)
         plugin = getattr(pas, plugin.getId())
 
-        infos=[info for info in pas.plugins.listPluginTypeInfo()
+        infos = [info for info in pas.plugins.listPluginTypeInfo()
                if plugin.testImplements(info["interface"])]
         plugin.manage_activateInterfaces([info["id"] for info in infos])
         for info in infos:
             for i in range(len(pas.plugins.listPluginIds(info["interface"]))):
                 pas.plugins.movePluginsUp(info["interface"], [plugin.getId()])
-
