10ec1b42a2e97cd9b982077cb17d6d8103335874
Wolfgang Thomas <thomas@syslab.com>
2017-08-02 17:03:02 +0200

    re-order imports, pep8

--- plonetheme/nuplone/z3cform/widget.py
+++ plonetheme/nuplone/z3cform/widget.py
@@ -1,23 +1,24 @@
-from zope.component import getMultiAdapter
-from zope.component import adapter
-from zope.interface import implementer
 from five import grok
-from ZPublisher.HTTPRequest import FileUpload
-from zope.schema.interfaces import IChoice
+from plone.formwidget.namedfile.widget import NamedFileWidget
+from plone.formwidget.namedfile.widget import NamedImageWidget
+from plone.namedfile.interfaces import INamedFileField
+from plone.namedfile.interfaces import INamedImageField
 from plonetheme.nuplone.z3cform.interfaces import INuPloneFormLayer
 from plonetheme.nuplone.z3cform.utils import getVocabulary
-from z3c.form.interfaces import IFieldWidget
-from z3c.form.interfaces import IMultiWidget
-from z3c.form.interfaces import IDataManager
-from z3c.form.interfaces import NOVALUE
-from z3c.form.widget import FieldWidget
 from z3c.form.browser.file import FileWidget
 from z3c.form.browser.radio import RadioWidget
 from z3c.form.browser.select import SelectWidget
-from plone.namedfile.interfaces import INamedImageField
-from plone.namedfile.interfaces import INamedFileField
-from plone.formwidget.namedfile.widget import NamedImageWidget
-from plone.formwidget.namedfile.widget import NamedFileWidget
+from z3c.form.interfaces import IDataManager
+from z3c.form.interfaces import IFieldWidget
+from z3c.form.interfaces import IMultiWidget
+from z3c.form.interfaces import NOVALUE
+from z3c.form.widget import FieldWidget
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+from zope.schema.interfaces import IChoice
+from ZPublisher.HTTPRequest import FileUpload
+
 
 class SingleRadioWidget(RadioWidget):
     """Variant of the z3c.form radio widget which does not pretend a radio
@@ -32,20 +33,18 @@ class SingleRadioWidget(RadioWidget):
     def update(self):
         super(SingleRadioWidget, self).update()
         for item in self.items:
-            item["name"]=item["name"].split(":list", 1)[0]
-
+            item["name"] = item["name"].split(":list", 1)[0]
 
 
 @adapter(IChoice, INuPloneFormLayer)
 @implementer(IFieldWidget)
 def ChoiceWidgetFactory(field, request):
-    vocabulary=getVocabulary(field)
-    if vocabulary is None or len(vocabulary)>4:
-        widget=SelectWidget
+    vocabulary = getVocabulary(field)
+    if vocabulary is None or len(vocabulary) > 4:
+        widget = SelectWidget
     else:
-        widget=SingleRadioWidget
+        widget = SingleRadioWidget
     return FieldWidget(field, widget(request))
-
 
 
 class NewMultiWidgetEntry(grok.View):
@@ -55,7 +54,6 @@ class NewMultiWidgetEntry(grok.View):
     def render(self):
         widget = self.context.getWidget(0)
         return widget.render()
-
 
 
 class NicerNamedImageWidget(NamedImageWidget):
@@ -71,7 +69,7 @@ class NicerNamedImageWidget(NamedImageWidget):
             if self.ignoreContext:
                 return default
 
-            dm=getMultiAdapter((self.context, self.field,), IDataManager)
+            dm = getMultiAdapter((self.context, self.field,), IDataManager)
             return dm.get()
 
         # Note that we allow the user to upload an empty file.
