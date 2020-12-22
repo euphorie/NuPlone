4b0e89427be5f9e6aa5892dd4b5a6acad453de06
Wolfgang Thomas <thomas@syslab.com>
2020-12-22 15:17:52 +0100

    properly de-grok @@contact, fixes 68d7480c5ae83536349e14ef8f4f2e73a04710e0

--- plonetheme/nuplone/browser/contact.py
+++ plonetheme/nuplone/browser/contact.py
@@ -4,7 +4,6 @@ from plone.directives import form
 from plonetheme.nuplone import MessageFactory as _
 from plonetheme.nuplone.utils import createEmailTo
 from plonetheme.nuplone.z3cform.form import FieldWidgetFactory
-from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
 from Products.MailHost.MailHost import MailHostError
 from Products.statusmessages.interfaces import IStatusMessage
@@ -20,9 +19,7 @@ import socket
 
 log = logging.getLogger(__name__)
 
-TextSpan7 = FieldWidgetFactory(
-    "z3c.form.browser.text.TextFieldWidget", klass="span-7"
-)
+TextSpan7 = FieldWidgetFactory("z3c.form.browser.text.TextFieldWidget", klass="span-7")
 TextLines4Rows = FieldWidgetFactory(
     "z3c.form.browser.textlines.TextLinesFieldWidget", rows=4
 )
@@ -32,23 +29,22 @@ class IContact(form.Schema):
     name = schema.TextLine(
         title=_(u"label_your_name", default="Your name"), required=True
     )
-    form.widget(name="plonetheme.nuplone.skin.contact.TextSpan7")
+    form.widget(name="plonetheme.nuplone.browser.contact.TextSpan7")
 
     email = schema.ASCIILine(
         title=_("label_email", default="Email address"), required=True
     )
-    form.widget(email="plonetheme.nuplone.skin.contact.TextSpan7")
+    form.widget(email="plonetheme.nuplone.browser.contact.TextSpan7")
 
     subject = schema.TextLine(
         title=_(u"label_subject", default=u"Subject"), required=True
     )
-    form.widget(subject="plonetheme.nuplone.skin.contact.TextSpan7")
+    form.widget(subject="plonetheme.nuplone.browser.contact.TextSpan7")
 
     message = schema.Text(
         title=_("label_contact_text", default="Your message"), required=True
     )
-    form.widget(message="plonetheme.nuplone.skin.contact.TextLines4Rows")
-
+    form.widget(message="plonetheme.nuplone.browser.contact.TextLines4Rows")
 
 
 class ContactForm(form.SchemaForm):
@@ -58,14 +54,13 @@ class ContactForm(form.SchemaForm):
     label = _(u"header_contact", default="Contact")
     default_fieldset_label = None
 
-
     @property
     def email_from_name(self):
-        return api.portal.get_registry_record('plone.email_from_name')
+        return api.portal.get_registry_record("plone.email_from_name")
 
     @property
     def email_from_address(self):
-        return api.portal.get_registry_record('plone.email_from_address')
+        return api.portal.get_registry_record("plone.email_from_address")
 
     @buttonAndHandler(_("button_send", default="Send"), name="send")
     def handleSend(self, action):
@@ -80,15 +75,17 @@ class ContactForm(form.SchemaForm):
         subject = _(
             u"contact_mail_subject",
             default=u"Contact request: ${subject}",
-            mapping=data
+            mapping=data,
         )
         subject = translate(subject, context=self.request)
 
         email = createEmailTo(
-            data["name"], data["email"],
+            data["name"],
+            data["email"],
             siteconfig.get("contact.name", self.email_from_name),
             siteconfig.get("contact.email", self.email_from_address),
-            subject, data["message"]
+            subject,
+            data["message"],
         )
         mh = getToolByName(self.context, "MailHost")
         flash = IStatusMessage(self.request).addStatusMessage
@@ -96,26 +93,26 @@ class ContactForm(form.SchemaForm):
             mh.send(email)
         except MailHostError as e:
             log.error(
-                "MailHost error sending contact form for %s: %s",
-                data["email"], e
+                "MailHost error sending contact form for %s: %s", data["email"], e
             )
             flash(
                 _(
                     u"error_contactmail",
-                    u"An error occured while processing your contact request. Please try again later."  # noqa: E501
-                ), "error"
+                    u"An error occured while processing your contact request. Please try again later.",  # noqa: E501
+                ),
+                "error",
             )
             return
         except socket.error as e:
             log.error(
-                "Socket error sending contact form for %s: %s", data["email"],
-                e[1]
+                "Socket error sending contact form for %s: %s", data["email"], e[1]
             )
             flash(
                 _(
                     u"error_contactmail",
-                    u"An error occured while processing your contact request. Please try again later."  # noqa: E501
-                ), "error"
+                    u"An error occured while processing your contact request. Please try again later.",  # noqa: E501
+                ),
+                "error",
             )
             return
 
