374ca0267f14e69a1bfbca7a2c92819ff81f87a3
Johannes Raggam <thetetet@gmail.com>
2022-01-12 18:31:56 +0100

    scr-1653 - Remove unused resources.

--- plonetheme/nuplone/skin/templates/libraries/ordering.js
+++
@@ -1,8 +0,0 @@
-// Easy ordering support for folders
-$(document).ready(function() {
-    $(".sortable").sortable({containment: "parent" });
-});
-$(".sortable").on("sortstop", function(e, ui) {
-    var order = $.map($(".sortable > *"), function(e) { return e.id;} );
-    $.post(plone.context_url+"/@@update-order", {order: order});
-});
