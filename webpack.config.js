d583267875a9b2a4d72816e387ef1b0f7a00f9e6
Johannes Raggam <thetetet@gmail.com>
2023-09-25 19:21:15 +0200

    JS: Upgrade config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
 process.traceDeprecation = true;
-const fs = require("fs");
+const mf_config = require("@patternslib/dev/webpack/webpack.mf");
+const package_json = require("./package.json");
+const package_json_patternslib = require("@patternslib/patternslib/package.json");
 const path = require("path");
-const patternslib_config = require("@patternslib/patternslib/webpack/webpack.config.js");
+const webpack_config =
+    require("@patternslib/dev/webpack/webpack.config").config;
 
-module.exports = async (env, argv) => {
+module.exports = () => {
     let config = {
         entry: {
-            "oira.cms.min": path.resolve(__dirname, "bundle-config.js"),
-            "bundle-polyfills.min": path.resolve(__dirname, "node_modules/@patternslib/patternslib/src/polyfills.js"), // prettier-ignore
+            "oira.cms.min": path.resolve(__dirname, "index.js"),
         },
     };
 
-    config = patternslib_config(env, argv, config);
+    config = webpack_config({
+        config: config,
+        package_json: package_json,
+    });
     config.output.path = path.resolve(
         __dirname,
-        "plonetheme/nuplone/static/bundle"
+        "plonetheme/nuplone/static/bundle",
+    );
+
+    config.plugins.push(
+        mf_config({
+            name: package_json.name,
+            remote_entry: config.entry["bundle.min"],
+            dependencies: {
+                ...package_json_patternslib.dependencies,
+                ...package_json.dependencies,
+            },
+        }),
     );
 
     if (process.env.NODE_ENV === "development") {
-        // Make sure we're using only one patternslib instance.
-        config.resolve.alias["@patternslib/patternslib"] = path.resolve(
-            __dirname,
-            "node_modules/@patternslib/patternslib"
-        );
-        // Make sure we're using only one jquery instance.
-        config.resolve.alias["jquery"] = path.resolve(
-            __dirname,
-            "node_modules/jquery"
-        );
+        config.devServer.static.directory = __dirname;
     }
 
     return config;
