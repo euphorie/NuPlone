b17b7760dd21dbfc2a43c91c68dfa6714e1e8ed5
Johannes Raggam <thetetet@gmail.com>
2023-09-25 19:23:38 +0200

    Register jquery globally.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import "@patternslib/patternslib/webpack/module_federation";
 // The next import needs to be kept with brackets, otherwise we get this error:
 // "Shared module is not available for eager consumption."
 import("./bundle-config");
+
+// Register Bootstrap and jQuery gloablly
+async function register_global_libraries() {
+    // Register jQuery globally
+    const jquery = (await import("jquery")).default;
+    window.jQuery = jquery;
+    window.$ = jquery;
+}
+register_global_libraries();
